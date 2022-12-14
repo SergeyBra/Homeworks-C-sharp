c66d6582fabaeddbf333baba2eb33ea7bf961cec
Sergey B <brazhnikov_serge@mail.ru>
2022-12-14 21:27:19 +0300

    task 50

---
+++ Task50/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает индексы элемента в двумерном массиве,
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1,1 -> 9
+// 1,7 -> элемента с данными индексами в массиве нет
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+Console.WriteLine("Введите индекс строки");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс строки");
+int y = Convert.ToInt32(Console.ReadLine());
+
+FindElementInArray(x, y, array);
+
+void FindElementInArray(int x1, int y1, int[,] arr)
+{
+    Console.WriteLine (x1 < arr.GetLength(0) && y1 < arr.GetLength(1) && x1 >= 0 && y1 >= 0
+    ? arr[x1, y1]
+    : "элемента с данными индексами в массиве нет");
+}
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
