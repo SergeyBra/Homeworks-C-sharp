163fcf96306be9b6b12606de2dd1b9b745de8cf3
Sergey B <brazhnikov_serge@mail.ru>
2022-12-14 21:26:46 +0300

    task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число до которого может быть значение элемента массива");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+double[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    double[,] filledArray = new double[arrayRows, arrayColumns];
+   Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] =Math.Round(random.Next(minValue, maxValue) + random.NextDouble(),1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(double[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
